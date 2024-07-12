7c6a0f82325e4eb41157b57117dade84a0db8d98
Oh...Yeah!!! <614988210@qq.com>
2024-07-12 21:52:06 +0800

    请求访问request

---
+++ rabbit-shop/src/utils/http.ts
@@ -0,0 +1,41 @@
+/*
+ * @Author: Oh...Yeah!!! 614988210@qq.com
+ * @Date: 2024-07-12 20:48:20
+ * @LastEditors: Oh...Yeah!!! 614988210@qq.com
+ * @LastEditTime: 2024-07-12 20:48:27
+ * @FilePath: \rabbit-shop\src\utils\http.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// src/utils/http.ts
+
+// 请求基地址
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 拦截器配置
+const httpInterceptor = {
+  // 拦截前触发
+  invoke(options: UniApp.RequestOptions) {
+    // 1. 非 http 开头需拼接地址
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+    // 2. 请求超时
+    options.timeout = 10000
+    // 3. 添加小程序端请求头标识
+    options.header = {
+      'source-client': 'miniapp',
+      ...options.header,
+    }
+    // 4. 添加 token 请求头标识
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
+  },
+}
+
+// 拦截 request 请求
+uni.addInterceptor('request', httpInterceptor)
+// 拦截 uploadFile 文件上传
+uni.addInterceptor('uploadFile', httpInterceptor)
--- rabbit-shop/vite.config.ts
+++ rabbit-shop/vite.config.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: Oh...Yeah!!! 614988210@qq.com
+ * @Date: 2024-07-11 21:45:56
+ * @LastEditors: Oh...Yeah!!! 614988210@qq.com
+ * @LastEditTime: 2024-07-12 21:41:41
+ * @FilePath: \rabbit-shop\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +17,10 @@ export default defineConfig({
     sourcemap: process.env.NODE_ENV === 'development',
   },
   plugins: [uni()],
+  base: '/', //此处添加了base的路径
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
